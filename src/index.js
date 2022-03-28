f73065e19ed2fd22520b0b54045676f02c783b3c
Gonzalo Cabrera <cabrerafernandogonzalo@gmail.com>
2022-03-28 16:55:42 -0300

    Conversion logic added

--- src/index.js
+++ src/index.js
@@ -174,17 +174,16 @@ let example = {
     }
     }
 
-    const base_rates = ["USD","JPY","ARS","EUR","AUD","RUB","BRL","GBP","CAD","CNY"]
-    const FLAGS_DIR = "../node_modules/flag-icons/flags/4x3/"
+    const base_rates = ["USD","JPY","ARS","EUR","AUD","RUB","BRL","GBP","CAD","CNY"];
+    const FLAGS_DIR = "../node_modules/flag-icons/flags/4x3/";
+    const API_URL = "https://v6.exchangerate-api.com/v6/3e5663daf097fa61633ab4d0/";
     
     const $baseRatesSelect = document.querySelector("#base-rate-select");
     const $ratesList = document.querySelector("#rates--list");
     const $BTNConvert = document.querySelector("#convert-btn");
     const $FRM = document.querySelector("#frm");
     let base = document.querySelector("#frm")["base-rate"];
-    
-    
-    
+     
     
     function loadBaseRates (baseRatesSelect, baseRatesList){
       baseRatesList.forEach(rate => {
@@ -195,12 +194,16 @@ let example = {
         baseRatesSelect.appendChild(option);
       });
     }
-    
-    //const $infoBaseRate = document.querySelector("#info--base-rate");
 
-    //$infoBaseRate.innerHTML = `Exchange rates based on <b>${example.base_code}</b> at ${example.time_last_update_utc}`;
-    loadBaseRates($baseRatesSelect, base_rates)
-    Object.entries(example.conversion_rates).forEach(([k,v]) => {
+    function getConvertionRates (baseRate){
+      return fetch(`${API_URL}latest/${baseRate}`)
+      .then(res => res.json());         
+    }
+
+    function loadConvertionRates(conversionData, quantity){
+      $ratesList.innerHTML = "";
+      document.querySelector("main").style.display = "block";
+      Object.entries(conversionData.conversion_rates).forEach(([k,v]) => {
         let convData = document.createElement('article');
         convData.id = k;
         convData.className ="rates--list-item"
@@ -210,15 +213,23 @@ let example = {
           <span class="currency-code">${k}</span> <span class="currency-name">${(currency_data[k] && currency_data[k].name)}</span> 
         </div> 
         <div>
-          <span class="currency-symbol">${(currency_data[k] && currency_data[k].symbol.grapheme)}</span> ${v.toFixed(2)} 
+          <span class="currency-symbol">${(currency_data[k] && currency_data[k].symbol.grapheme)}</span> ${(v * quantity).toFixed(2)} 
         </div>
         `
         $ratesList.appendChild(convData);
-    });
+    });  
+    }
     
-    $FRM.addEventListener('submit', (event) =>{
-      event.preventDefault();      
+    loadBaseRates($baseRatesSelect, base_rates)
+    
+    $FRM.addEventListener('submit',async (event) =>{
+      event.preventDefault(); 
+      loadConvertionRates(await getConvertionRates(($FRM.base_rate.value && $FRM.base_rate.value)), Number($FRM.quantity.value));
     })
-    //<img src="https://raw.githubusercontent.com/stefangabos/world_countries/master/data/flags/48x48/${k.slice(0,2).toLowerCase()}.png" />
+
+    $baseRatesSelect.addEventListener('change', (e)=> 
+      document.querySelector('#base-rate-flag').src = `${FLAGS_DIR+e.target.value.slice(0,2).toLowerCase()}.svg`
+    );
+    
 
    
